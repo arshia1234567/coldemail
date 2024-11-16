a51a530c5cd44e96f308d755f139f0c2dcbf91ef
Arshia <arshiaa245@gmail.com>
2024-11-16 21:56:52 +0530

    added  secrets file

--- app/chains.py
+++ app/chains.py
@@ -1,15 +1,17 @@
 import os
+
+import streamlit as st
 from langchain_groq import ChatGroq
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.exceptions import OutputParserException
 from dotenv import load_dotenv
 
-load_dotenv()
 
+groq_api_key = st.secrets["GROQ_API_KEY"]
 class Chain:
     def __init__(self):
-        self.llm = ChatGroq(temperature=0, groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.1-70b-versatile")
+        self.llm = ChatGroq(temperature=0, groq_api_key="groq_api_key", model_name="llama-3.1-70b-versatile")
 
     def extract_jobs(self, cleaned_text):
         prompt_extract = PromptTemplate.from_template(
